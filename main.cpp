e42b26af4ec288c1e5196d7e11b003e20a78d13e
Katherine Shashkina <kshashkina@kse.org.ua>
2023-12-20 01:04:27 +0200

    Rendering and funcs calls in main

--- main.cpp
+++ main.cpp
@@ -368,3 +368,244 @@ public:
 
     bool gameOver = false;
 };
+
+class GameRendering{
+private:
+    sf::Font font;
+public:
+    void setFont() {
+        if (!font.loadFromFile("C:\\KSE IT\\oop_game\\zh-cn.ttf")) {
+            throw std::runtime_error("Failed to load font");
+        }
+    }
+    void addCursor(sf::RenderWindow& window) {
+        sf::Image cursorImage;
+        if (!cursorImage.loadFromFile("C:\\KSE IT\\sfml\\cursor.png")) {
+            throw std::runtime_error("Failed to load texture");
+        }
+
+        sf::Cursor cursor;
+        if (cursor.loadFromPixels(cursorImage.getPixelsPtr(), cursorImage.getSize(), sf::Vector2u(0, 0))) {
+            window.setMouseCursor(cursor);
+        } else {
+            throw std::runtime_error("Failed to load cursor");
+        }
+    }
+
+    void scoreText(sf::RenderWindow& window, ScoreManager& scoreManager) {
+        int score = scoreManager.getCurrentScore();
+        sf::Text scoreText;
+        scoreText.setFont(font);
+        scoreText.setString("Your score: " + std::to_string(score));
+        scoreText.setCharacterSize(24);
+        scoreText.setFillColor(sf::Color::White);
+        scoreText.setPosition(10, 10);
+        window.draw(scoreText);
+    }
+
+    void livesText(sf::RenderWindow& window, Spaceship& spaceship) {
+        int lives = spaceship.getLives();
+        sf::Text livesText;
+        livesText.setFont(font);
+        livesText.setString("Lives: " + std::to_string(lives));
+        livesText.setCharacterSize(24);
+        livesText.setFillColor(sf::Color::White);
+        livesText.setPosition(1800, 10);
+        window.draw(livesText);
+    }
+    void renderGame(sf::RenderWindow& window) {
+        GameManager gameInterface;
+        Spaceship spaceship;
+        Asteroid asteroid;
+        ScoreManager scoreManager;
+        GameRendering manager;
+
+        sf::Texture mainMenuBackgroundTexture, buttonTexture, gameBackgroundTexture;
+        if (!mainMenuBackgroundTexture.loadFromFile("C:\\KSE IT\\sfml\\mainpage.jpg") ||
+            !buttonTexture.loadFromFile("C:\\KSE IT\\sfml\\startgame.png") ||
+            !gameBackgroundTexture.loadFromFile("C:\\KSE IT\\oop_game\\background.jpg")){
+            throw std::runtime_error("Failed to load texture");
+        }
+        sf::Texture gameOverTexture;
+        sf::Sprite gameOverSprite;
+        if (!gameOverTexture.loadFromFile("C:\\KSE IT\\sfml\\gameover.jpg")) {
+            throw std::runtime_error("Failed to load texture");
+        }
+
+        sf::RectangleShape playAgainButton;
+        playAgainButton.setSize(sf::Vector2f(115.0f, 75.0f)); // размер кнопки
+        playAgainButton.setFillColor(sf::Color::Transparent); // цвет кнопки
+        playAgainButton.setPosition(815.0f, 840.0f); // позиция кнопки
+
+        sf::RectangleShape exitButton;
+        exitButton.setSize(sf::Vector2f(115.0f, 75.0f));
+        exitButton.setFillColor(sf::Color::Transparent);
+        exitButton.setPosition(1000.0f, 840.0f);
+
+        gameOverSprite.setTexture(gameOverTexture);
+        gameOverSprite.setColor(sf::Color(255, 255, 255, 0)); // Изначально спрайт полностью прозрачен
+        gameOverSprite.setPosition(0, 0);
+
+        sf::Sprite mainMenuBackground(mainMenuBackgroundTexture);
+        sf::Sprite background(gameBackgroundTexture);
+        sf::Sprite button(buttonTexture);
+        button.setPosition(780.0f, 405.0f);
+        sf::Vector2u buttonTextureSize = buttonTexture.getSize();
+        float scaleX = 370.0f / buttonTextureSize.x;
+        float scaleY = 140.0f / buttonTextureSize.y;
+        button.setScale(scaleX, scaleY);
+
+        int highScore = scoreManager.loadHighScore();
+        sf::Text highestScore;
+        highestScore.setFont(font);
+        std::string hexColor = "#25335a";
+        unsigned int rgb = std::stoul(hexColor.erase(0, 1), nullptr, 16); // Convert hex to integer
+        sf::Color customColor((rgb >> 16) & 0xFF, (rgb >> 8) & 0xFF, rgb & 0xFF);
+        highestScore.setString("Highest score\n         " + std::to_string(highScore));
+        highestScore.setCharacterSize(28);
+        highestScore.setFillColor(customColor);
+        highestScore.setPosition(865, 735);
+
+        bool gameStarted = false;
+        bool inTransition = false;
+        float transitionTime = 3.0f;
+        float currentTransitionTime = 0.0f;
+        float gameOverFadeInTime = 3.0f;
+        float gameOverFadeInTimer = 0.0f;
+        setFont();
+        auto restartGame = [&]() {
+            scoreManager.resetScore();
+            spaceship.setLives(3);
+            asteroid.meteors.clear();
+            spaceship.projectiles.clear();
+            asteroid.meteorDirections.clear();
+            asteroid.meteorSpeeds.clear();
+            gameStarted = true;
+            gameInterface.gameOver = false;
+            asteroid.currentMeteorSpawnTime = 0.0f;
+            gameOverFadeInTime = 2.0f;
+            gameOverFadeInTimer = 0.0f;
+        };
+
+        while (window.isOpen()) {
+            sf::Event event;
+            while (window.pollEvent(event)) {
+                if (event.type == sf::Event::Closed) {
+                    window.close();
+                }
+                if (!gameStarted && !inTransition && event.type == sf::Event::MouseButtonPressed) {
+                    if (button.getGlobalBounds().contains(window.mapPixelToCoords(sf::Mouse::getPosition(window)))) {
+                        inTransition = true;
+                    }
+                }
+            }
+            if (inTransition) {
+                currentTransitionTime += 1.0f / 60;
+                if (currentTransitionTime >= transitionTime) {
+                    inTransition = false;
+                    gameStarted = true;
+                    currentTransitionTime = transitionTime;
+                }
+            }
+            float alpha = currentTransitionTime / transitionTime;
+            mainMenuBackground.setColor(sf::Color(255, 255, 255, static_cast<sf::Uint8>(255 * (1 - alpha))));
+            background.setColor(sf::Color(255, 255, 255, static_cast<sf::Uint8>(255 * alpha)));
+
+            window.clear();
+            if (!gameStarted || inTransition) {
+                window.draw(mainMenuBackground);
+                window.draw(highestScore);
+                if (!inTransition) {
+                    window.draw(button);
+                }
+            }
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Space)) {
+                spaceship.shootProjectile();
+            }
+            if (gameStarted || inTransition){
+                spaceship.move(window);
+                asteroid.update(window);
+                asteroid.generate(window);
+                gameInterface.checkCollisions(spaceship, asteroid, scoreManager);
+                gameInterface.setTextureBack(spaceship);
+                gameInterface.checkProjectileCollisions(spaceship, asteroid, scoreManager);
+                spaceship.updateProjectiles(window);
+
+                window.clear();
+                window.draw(background);
+                scoreText(window, scoreManager);
+                livesText(window, spaceship);
+                window.draw(spaceship.getSprite());
+                asteroid.draw(window);
+
+                for (const auto& projectile : spaceship.projectiles) {
+                    window.draw(projectile);
+                }
+                if (gameInterface.getGameStatus()){
+                    gameOverFadeInTimer += 0.5f / 60.0f;
+                    float alpha = (gameOverFadeInTimer / gameOverFadeInTime) * 255.0f;
+                    if (alpha > 255.0f) alpha = 255.0f;
+                    gameOverSprite.setColor(sf::Color(255, 255, 255, static_cast<sf::Uint8>(alpha)));
+                    window.clear();
+                    if (sf::Mouse::isButtonPressed(sf::Mouse::Left)) {
+                        sf::Vector2i mousePos = sf::Mouse::getPosition(window);
+
+                        if (playAgainButton.getGlobalBounds().contains(static_cast<sf::Vector2f>(mousePos))) {
+                            restartGame();
+                        }
+                        if (exitButton.getGlobalBounds().contains(static_cast<sf::Vector2f>(mousePos))) {
+                            window.close();
+                        }
+                    }
+                    if (scoreManager.getCurrentScore() > highScore) {
+                        scoreManager.saveHighScore();
+                        highScore = scoreManager.getCurrentScore();
+                    }
+                }
+            }
+            if (gameInterface.getGameStatus()) {
+                sf::Text scoreText;
+                scoreText.setFont(font);
+                scoreText.setString(std::to_string(scoreManager.getCurrentScore()));
+                scoreText.setCharacterSize(24);
+                scoreText.setFillColor(sf::Color::White);
+                scoreText.setPosition(880, 650);
+
+                sf::Text highestScore;
+                highestScore.setFont(font);
+                highestScore.setString(std::to_string(highScore));
+                highestScore.setCharacterSize(24);
+                highestScore.setFillColor(sf::Color::White);
+                highestScore.setPosition(1025, 650);
+
+                sf::Text newRec;
+                newRec.setFont(font);
+                newRec.setString("New highest score!");
+                newRec.setCharacterSize(24);
+                newRec.setFillColor(sf::Color::White);
+                newRec.setPosition(850, 680);
+
+
+
+                window.draw(playAgainButton);
+                window.draw(exitButton);
+                window.draw(gameOverSprite);
+                window.draw(scoreText);
+                window.draw(highestScore);
+                if (scoreManager.getCurrentScore() == highScore) {
+                    window.draw(newRec);
+                }
+            }
+            window.display();
+        }
+    }
+
+};
+
+int main() {
+    sf::RenderWindow window(sf::VideoMode(1920, 1080), "Asteroid");
+    GameRendering gameManager;
+    gameManager.addCursor(window);
+    gameManager.renderGame(window);
+    return 0;
+}
